873f12d4ff699d5ccc0897f2143969ffd66a84f2
Varun <varun@niiu.de>
2017-04-25 12:08:20 +0530

    Initial Commit

---
+++ VdoboxPOC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VdoboxPOC
+//
+//  Created by Varun on 25/04/17.
+//  Copyright © 2017 Diet Code. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
